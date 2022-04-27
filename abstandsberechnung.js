99d9579b92d2721f2cba45171b5533baf41b71d1
tkrumrei <tkrumrei@uni-muenster.de>
2022-04-27 20:40:03 +0200

    .

--- abstandsberechnung.js
+++ abstandsberechnung.js
@@ -1,29 +1,25 @@
 "use strict";
 
-// Array für die Ausgabe erstellen mit Name der Stadt und Abstand
-var citiesDistance = [
-    ["Köln", 0.0],
-    ["Amsterdam", 0.0],
-    ["Kassel", 0.0],
-    ["Barcelona", 0.0],
-    ["Tunis", 0.0],
-    ["Kyoto", 0.0],
-    ["Bucharest", 0.0],
-    ["Graz", 0.0],
-    ["Kairo", 0.0],
-    ["Dublin", 0.0],
-    ["Oslo", 0.0],
-];
+var aPosition = {
+    "type": "Point",
+    "coordinates": [0,0]
+}
+
+aPosition.coordinates[0] = point[0];
+aPosition.coordinates[1] = point[1];
 
 // Punkt point als ersten Punkt für Formel speichern
-var lat1 = point[1];
-var lon1 = point[0];
+var lat1 = aPosition.coordinates[1];
+var lon1 = aPosition.coordinates[0];
 
-// Berechnung der Abstände von Städten zu Punkt mit for-Schleife 
-for(var i = 0; i < cities.length; i++) {
-    // Punkte von cities-Array als zweiten Punkt speichern 
-    var lat2 = cities[i][1];
-    var lon2 = cities[i][0];
+var ausgabeAB = "";
+var poisDistance = new Array(pois.features.length);
+
+// Berechnung der Abstände von Punkten(pois) zu Punkt mit for-Schleife 
+for(var i = 0; i < pois.features.length; i++) {
+    // Punkte von pois-Array als zweiten Punkt speichern 
+    var lat2 = pois.features[i].geometry.coordinates[1];
+    var lon2 = pois.features[i].geometry.coordinates[0];
     
     const R = 6371e3; // metres
     const φ1 = lat1 * Math.PI/180; // φ, λ in radians
@@ -32,27 +28,112 @@ for(var i = 0; i < cities.length; i++) {
     const Δλ = (lon2-lon1) * Math.PI/180;
 
     const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-          Math.cos(φ1) * Math.cos(φ2) *
-          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-    var d = Math.round((R * c / 1000) * 10) / 10; // in km umgerechnet und auf eine Nachkommastelle gerundet
+    var d = Math.round(R * c * 10) / 10; // in m umgerechnet und auf eine Nachkommastelle gerundet
 
-    // In array cities distance eintragen
-    citiesDistance[i][1] = d;
+    // Mehrdimensionales Array erstellen
+    poisDistance[i] = new Array(2);
+
+    // In array pois distance eintragen
+    poisDistance[i][1] = d;
+    poisDistance[i][0] = pois.features[i].properties.name;
+
+    
 }
 
-// Array citiesDistance aufsteigend sortieren
+// Array poisDistance aufsteigend sortieren
 //Quelle: https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/array/sort
-citiesDistance.sort(zweiteSpalteSortieren);
+poisDistance.sort(zweiteSpalteSortieren);
 // Funktion für zweite Spalte sortieren 
-//gibt entweder -1, 0 oder 1 zurück und sort() tauscht dann die beiden ausgewählten Spalten
+// gibt entweder -1, 0 oder 1 zurück und sort() tauscht dann die beiden ausgewählten Spalten
+// Arrow-Funktion
+//let zweiteSpalteSortieren = (a, b) => a[1] - b[1];
 function zweiteSpalteSortieren (a, b){
     return a[1] - b[1];
+    }
+
+// Textausgabe von aufsteigend sortierten gegebenen Punkten als Text mit Zeilenumbruch (in HTML) eingefügen
+for(var i = 0; i < poisDistance.length; i++) {
+ausgabeAB = ausgabeAB + poisDistance[i][0]+ ": " + poisDistance[i][1] + "m" + "<br />";
 }
 
-// Textausgabe von aufsteigend sortierten citiesDistance als Text mit Zeilenumbruch (in HTML) eingefügen
-var ausgabeAB = "";
-for(var i = 0; i < citiesDistance.length; i++) {
-    ausgabeAB = ausgabeAB + citiesDistance[i][0] + ": " +  citiesDistance[i][1] + "km" + "<br />";
+
+// Abstand aktueller Standort Berechnung
+function abstandBerechnen() {
+
+    var lat1 = aPosition.coordinates[1];
+    var lon1 = aPosition.coordinates[0];
+
+    ausgabeAB = "";
+    var poisDistance = new Array(pois.features.length);
+
+    // Berechnung der Abstände von Punkten(pois) zu Punkt mit for-Schleife 
+    for(var i = 0; i < pois.features.length; i++) {
+        // Punkte von pois-Array als zweiten Punkt speichern 
+        var lat2 = pois.features[i].geometry.coordinates[1];
+        var lon2 = pois.features[i].geometry.coordinates[0];
+        
+        const R = 6371e3; // metres
+        const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+        const φ2 = lat2 * Math.PI/180;
+        const Δφ = (lat2-lat1) * Math.PI/180;
+        const Δλ = (lon2-lon1) * Math.PI/180;
+
+        const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        var d = Math.round(R * c * 10) / 10; // in m umgerechnet und auf eine Nachkommastelle gerundet
+
+        // Mehrdimensionales Array erstellen
+        poisDistance[i] = new Array(2);
+
+        // In array pois distance eintragen
+        poisDistance[i][1] = d;
+        poisDistance[i][0] = pois.features[i].properties.name;
+
+        
+    }
+    // Array poisDistance aufsteigend sortieren
+    //Quelle: https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/array/sort
+    poisDistance.sort(zweiteSpalteSortieren);
+    // Funktion für zweite Spalte sortieren 
+    //gibt entweder -1, 0 oder 1 zurück und sort() tauscht dann die beiden ausgewählten Spalten
+    function zweiteSpalteSortieren (a, b){
+    return a[1] - b[1];
+    }
+
+    // Textausgabe von aufsteigend sortierten gegebenen Punkten als Text mit Zeilenumbruch (in HTML) eingefügen
+    for(var i = 0; i < poisDistance.length; i++) {
+    ausgabeAB = ausgabeAB + poisDistance[i][0]+ ": " + poisDistance[i][1] + "m" + "<br />";
+    }
+
 }
+
+
+
+               
+
+// Funktion für Ermittlung des Standorts plus eintragen in das Eingabefeld
+ function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                document.getElementById("mPosition").innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function showPosition(position) {
+            // GeoJSON Point erstellen
+            aPosition.coordinates[0] = position.coords.longitude;
+            aPosition.coordinates[1] = position.coords.latitude;
+            
+            // mit Stringify GeoJSON in Eingabefeld eintragen
+            document.getElementById("mPosition").innerHTML = JSON.stringify(aPosition);
+        }
+
+
