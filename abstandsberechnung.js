f01fa2d44d91f0fd6a5b3b47cd5c88a78148a9f6
tkrumrei <tkrumrei@uni-muenster.de>
2022-04-27 22:36:13 +0200

    javadocs usw auskommentieren

--- abstandsberechnung.js
+++ abstandsberechnung.js
@@ -1,26 +1,37 @@
-"use strict";
 /**
- * @author Tobias Krumrein
- * @description 
+ * @author      Tobias Krumrein
+ * @description Diese HTML-Seite berechnet den Abstand zwischen dem gegebenen Punkt aus der Datei point.js und den
+ *              Punkten aus poi.js, welche in einem GeoJSON-Format aufgeschrieben sind. Alternativ lässt sich auch der
+ *              aktuelle Standort bestimmen, mit dem man dann auch den Abstand zu den Punkten poi.js berechnen kann.
  */
 
-var ausgabeAB = "";
+ "use strict";
 
+// Titel ändern mit JavaScript
+ document.title = "Aufgabe 2 Standorte und Points-of-interest";
+// Eine leere Variabel, damit man die Distanzen zwischen den Punkten später ausgeben kann
+var ausgabeAB = "";
+// Einen GeoJSON Point erstellen für die Koordinaten
 var aPosition = {
     "type": "Point",
     "coordinates": [0,0]
 }
 
-function abstandBerechnenPunkt () {
+/**
+ * @function    abstandBerechnenPunkt()
+ * @description Diese Funktion berechnet den Abstand zwischen dem gegebenen Punkt aus der Datei point.js und den
+ *              Punkten aus poi.js, welche in einem GeoJSON-Format aufgeschrieben sind. * 
+ */
+function abstandBerechnenPunkt() {
+    // Der Punkt wird in aPosition eingeladen 
     aPosition.coordinates[0] = point[0];
     aPosition.coordinates[1] = point[1];
 
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Punkt point als ersten Punkt für Formel speichern
     var lat1 = aPosition.coordinates[1];
     var lon1 = aPosition.coordinates[0];
 
+    // poisDistance Array erstellen mit Länge von pois.js 
     var poisDistance = new Array(pois.features.length);
 
     // Berechnung der Abstände von Punkten(pois) zu Punkt mit for-Schleife 
@@ -40,16 +51,15 @@ function abstandBerechnenPunkt () {
             Math.sin(Δλ/2) * Math.sin(Δλ/2);
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-        var d = Math.round(R * c * 10) / 10; // in m umgerechnet und auf eine Nachkommastelle gerundet
+        var d = Math.round(R * c * 10) / 10;  // auf eine Nachkommastelle gerundet
+        
 
-        // Mehrdimensionales Array erstellen
+        // Mehrdimensionales Array erstellen, um in poisDistance die Entfernung und den Namen einzutragen
         poisDistance[i] = new Array(2);
 
-        // In array pois distance eintragen
+        // In Array pois distance eintragen
         poisDistance[i][1] = d;
-        poisDistance[i][0] = pois.features[i].properties.name;
-
-        
+        poisDistance[i][0] = pois.features[i].properties.name;        
     }
 
     // Array poisDistance aufsteigend sortieren
@@ -57,11 +67,14 @@ function abstandBerechnenPunkt () {
     poisDistance.sort(zweiteSpalteSortieren);
     // Funktion für zweite Spalte sortieren 
     // gibt entweder -1, 0 oder 1 zurück und sort() tauscht dann die beiden ausgewählten Spalten
-    // Arrow-Funktion
-    //let zweiteSpalteSortieren = (a, b) => a[1] - b[1];
+    /* Arrow-Funktion:
+       let zweiteSpalteSortieren = (a, b) => a[1] - b[1];
+       Funktioniert leider nicht
+    */
     function zweiteSpalteSortieren (a, b){
         return a[1] - b[1];
         }
+        
 
     // Textausgabe von aufsteigend sortierten gegebenen Punkten als Text mit Zeilenumbruch (in HTML) eingefügen
     for(var i = 0; i < poisDistance.length; i++) {
@@ -69,17 +82,24 @@ function abstandBerechnenPunkt () {
     }
 }
 
+// Aufruf der Methode aufstandBerechnePunkt()
 abstandBerechnenPunkt();
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Abstand aktueller Standort Berechnung
+/**
+ * @function    abstandBerechnen()
+ * @description Diese Funktion berechnet den Abstand zwischen dem aktuellen Punkt der vorher in aPosition reingeladen wurde und den
+ *              Punkten aus poi.js, welche in einem GeoJSON-Format aufgeschrieben sind.
+ */
 function abstandBerechnen() {
-
+    // Aktuellen Punkt als ersten Punkt für Formel speichern 
     var lat1 = aPosition.coordinates[1];
     var lon1 = aPosition.coordinates[0];
 
+    // ausgabeAB leeren
     ausgabeAB = "";
+    // poisDistance Array erstellen mit Länge von pois.js
     var poisDistance = new Array(pois.features.length);
 
     // Berechnung der Abstände von Punkten(pois) zu Punkt mit for-Schleife 
@@ -99,7 +119,7 @@ function abstandBerechnen() {
             Math.sin(Δλ/2) * Math.sin(Δλ/2);
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-        var d = Math.round(R * c * 10) / 10; // in m umgerechnet und auf eine Nachkommastelle gerundet
+        var d = Math.round(R * c * 10) / 10; // auf eine Nachkommastelle gerundet
 
         // Mehrdimensionales Array erstellen
         poisDistance[i] = new Array(2);
@@ -127,7 +147,10 @@ function abstandBerechnen() {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////               
-// Funktion für Ermittlung des Standorts plus eintragen in das Eingabefeld
+/**
+ * @function    getLocation()
+ * @description Funktion für Ermittlung des Standorts
+ */
  function getLocation() {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(showPosition);
@@ -135,14 +158,18 @@ function abstandBerechnen() {
                 document.getElementById("mPosition").innerHTML = "Geolocation is not supported by this browser.";
             }
         }
-
-        function showPosition(position) {
-            // GeoJSON Point erstellen
-            aPosition.coordinates[0] = position.coords.longitude;
-            aPosition.coordinates[1] = position.coords.latitude;
-            
-            // mit Stringify GeoJSON in Eingabefeld eintragen
-            document.getElementById("mPosition").innerHTML = JSON.stringify(aPosition);
-        }
+/**
+ * @function showPosition()
+ * @description Funktion ermittelt den aktuellen Standort und trägt ihn dann in das Eingabefeld mPosition ein
+ * @param {*} position gibt die position an
+ */
+function showPosition(position) {
+    // GeoJSON Point erstellen
+    aPosition.coordinates[0] = position.coords.longitude;
+    aPosition.coordinates[1] = position.coords.latitude;
+    
+    // mit Stringify GeoJSON in Eingabefeld eintragen
+    document.getElementById("mPosition").innerHTML = JSON.stringify(aPosition);
+}
 
 
